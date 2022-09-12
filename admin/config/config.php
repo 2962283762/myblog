89edebe5598b4f4443c95bb9d50c043d245e4c90
2962283762 <53593279+2962283762@users.noreply.github.com>
2022-09-12 16:33:34 +0800

    Update config.php

--- admin/config/config.php
+++ admin/config/config.php
@@ -1,8 +1,8 @@
 <?php
-const HOST="162.14.107.142";
-const USER="root";
-const PASSWD = "1937dzl.";
-const DATABASE = "blog";
+const HOST="";
+const USER="";
+const PASSWD = "";
+const DATABASE = "";
 function connecttodb(){
     $db = mysqli_connect(HOST,USER,PASSWD,DATABASE);
     if(!$db){
